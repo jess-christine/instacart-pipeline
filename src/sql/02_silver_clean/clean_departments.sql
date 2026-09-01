2699dac46ed18b5fc9c5e6f4e43aecdf1a8f3460
alleanna.villan@ftwfoundation.org <alleanna.villan@ftwfoundation.org>
2026-09-01 14:31:16 +0000

    Updated clean_department

--- src/sql/02_silver_clean/clean_departments.sql
+++ src/sql/02_silver_clean/clean_departments.sql
@@ -1,29 +1,28 @@
- --Create the clean table for department
-CREATE TABLE IF NOT EXISTS instacart.instacart_clean.clean_departments(
+-- Create the clean table for department
+CREATE TABLE IF NOT EXISTS instacart.instacart_clean.departments_clean (
     department_id INT PRIMARY KEY,
     department VARCHAR(100) UNIQUE,
     ingestion_timestamp TIMESTAMP,
     ingestion_date DATE
 );
 
--- Insert cleaned data from raw table (this step makes sure that new entries from raw table will be cleaned before inserting into clean table)
-INSERT INTO instacart.instacart_clean.clean_departments(department_id, department, ingestion_timestamp, ingestion_date)
-SELECT DISTINCT
-    department_id,
-    LOWER(TRIM(department)) AS department,
-    ingestion_timestamp,
-    CAST(ingestion_timestamp AS DATE) AS ingestion_date
-FROM instacart.instacart_raw.departments_raw
-WHERE department_id IS NOT NULL;
-
---Query with CTE for future transformations
-WITH standardized AS (
-    SELECT 
+-- Merge cleaned data from raw table
+MERGE INTO instacart.instacart_clean.departments_clean AS target
+USING (
+    SELECT DISTINCT
         department_id,
         LOWER(TRIM(department)) AS department,
         ingestion_timestamp,
-        ingestion_date
-    FROM instacart.instacart_clean.clean_departments
-)
-SELECT *
-FROM standardized;
+        CAST(ingestion_timestamp AS DATE) AS ingestion_date
+    FROM instacart.instacart_raw.departments_raw
+    WHERE department_id IS NOT NULL
+) AS source
+ON target.department_id = source.department_id
+WHEN MATCHED THEN
+    UPDATE SET
+        target.department = source.department,
+        target.ingestion_timestamp = source.ingestion_timestamp,
+        target.ingestion_date = source.ingestion_date
+WHEN NOT MATCHED THEN
+    INSERT (department_id, department, ingestion_timestamp, ingestion_date)
+    VALUES (source.department_id, source.department, source.ingestion_timestamp, source.ingestion_date);
