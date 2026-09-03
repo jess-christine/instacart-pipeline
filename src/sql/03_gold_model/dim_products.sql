8f21de54f98121aa97eb7e1dad527cbcd3c3fad5
joan.maquinano@ftwfoundation.org <joan.maquinano@ftwfoundation.org>
2026-09-03 02:04:49 +0000

    Removed quality checks from dim_products and will be moved to the appropriate .sql file for quality check.

--- src/sql/03_gold_model/dim_products.sql
+++ src/sql/03_gold_model/dim_products.sql
@@ -57,38 +57,12 @@ LEFT JOIN instacart.instacart_clean.departments_clean d
 
 
 /*
-Validation
+Post-Load Validation
 
-Expected:
-- source_count = dim_count
-- duplicate_product_ids = 0
-- unmatched_aisle_records = 0
-- unmatched_department_records = 0
+Detailed data quality assertions are maintained in
+data_quality_checks.sql and should be executed before
+promoting DimProducts to downstream reporting layers.
 */
-
-SELECT
-    (SELECT COUNT(*)
-     FROM instacart.instacart_clean.products_clean) AS source_count,
-
-    (SELECT COUNT(*)
-     FROM instacart.instacart_gold.dim_products) AS dim_count,
-
-    (SELECT COUNT(*)
-     FROM (
-         SELECT product_id
-         FROM instacart.instacart_gold.dim_products
-         GROUP BY product_id
-         HAVING COUNT(*) > 1
-     )) AS duplicate_product_ids,
-
-    (SELECT COUNT(*)
-     FROM instacart.instacart_gold.dim_products
-     WHERE aisle_id IS NULL) AS unmatched_aisle_records,
-
-    (SELECT COUNT(*)
-     FROM instacart.instacart_gold.dim_products
-     WHERE department_id IS NULL) AS unmatched_department_records;
-
 
 /*
 Since this dimension uses SCD Type 1,
